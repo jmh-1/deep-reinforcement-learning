18b18627e92a4181539f393947787e2150179f63
a <>
2021-07-22 22:41:41 -0400

    tmp

--- p1_navigation/dqn_agent.py
+++ p1_navigation/dqn_agent.py
@@ -26,7 +26,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed, gamma=GAMMA, tau=TAU, lr=LR):
+    def __init__(self, state_size, action_size, seed, gamma=GAMMA, tau=TAU, lr=LR, qNetwork=QNetwork):
         """Initialize an Agent object.
         
         Params
@@ -43,8 +43,8 @@ class Agent():
         self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_local = qNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_target = qNetwork(state_size, action_size, seed).to(device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.lr)
 
         # Replay memory
