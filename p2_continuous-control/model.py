c1885072c1ae5fd8a75f436b71517e20b2bfb755
a <>
2021-08-31 22:02:20 -0400

    add batch norm

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from IPython.core.debugger import set_trace
 
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
@@ -27,6 +28,8 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -37,7 +40,9 @@ class Actor(nn.Module):
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
+        x = self.bn1(x)
         x = F.relu(self.fc2(x))
+        x = self.bn2(x)
         return F.tanh(self.fc3(x))
 
 
@@ -59,6 +64,8 @@ class Critic(nn.Module):
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -69,6 +76,8 @@ class Critic(nn.Module):
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         xs = F.relu(self.fcs1(state))
+        xs = self.bn1(xs)
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
+        x = self.bn2(x)
         return self.fc3(x)
