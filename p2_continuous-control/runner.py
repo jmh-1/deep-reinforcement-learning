ac936c19011963852476b4f323fc89184a53a7df
a <>
2021-09-06 17:19:52 -0400

    restore 1w

--- p2_continuous-control/runner.py
+++ p2_continuous-control/runner.py
@@ -5,16 +5,25 @@ import timeit
 import matplotlib.pyplot as plt
 import torch
 
-def run(env, agent, n_episodes=3000, brain_name=""):
-    scores_deque = deque(maxlen=100)  
-    scores = []                        
+def run(env, agent, n_episodes=2000, max_t=5000, brain_name="", breakWhenSolved=True, state_file_name="checkpoint.pth"):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    solved = False
     for i_episode in range(1, n_episodes+1):
-        
         env_info = env.reset(train_mode=True)[brain_name] 
         state = env_info.vector_observations[0]           
-        agent.reset()
         score = 0
-        while True:
+        for t in range(max_t):
             action = agent.act(state)
             env_info = env.step(action)[brain_name]        
             next_state = env_info.vector_observations[0]   
@@ -29,17 +38,27 @@ def run(env, agent, n_episodes=3000, brain_name=""):
             if np.any(done):
                 break 
 
-        scores_deque.append(score)    
+        scores_window.append(score)    
         scores.append(score)          
-        avg_score = np.mean(scores_deque)
-
-        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, avg_score, score), end="")
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_window), score), end="")
         if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=30.0 and not solved:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-        if avg_score>=30:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_deque)))
-            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            break
+            if breakWhenSolved:
+                break
+            solved = True
     return scores
+
+def plot_scores(scores):
+    n=100
+    running_average = np.correlate(scores, np.ones(n)/n, mode='valid')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores, label='Score')
+    plt.plot(np.arange(len(running_average)) + n - 1 , running_average, label=f'Average Score over prior {n} episodes')
+    plt.xlabel('Episode #')
+    ax.legend()
+    plt.show()
